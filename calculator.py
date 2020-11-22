f3ef538fcd3d3808184de718e9ff9cb8285b4fac
E DeGroot <ElizabethADeGroot@users.noreply.github.com>
2020-11-21 19:24:39 -0500

    updating tests add

--- calculator.py
+++ calculator.py
@@ -3,8 +3,8 @@ Calculator library containing basic math operations.
 """
 
 
-def add(first_term, second_term):
-    return first_term + second_term
+def add(a, b):
+    return a + b
 
 
 def subtract(first_term, second_term):
--- test_calculator.py
+++ test_calculator.py
@@ -4,12 +4,19 @@ Unit tests for the calculator library
 
 import unittest
 import calculator
+ 
 
 class TestStringMethods(unittest.TestCase):
 
 
     def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+        a = 2
+        b = 2 
+        assert 4 == calculator.add(a, b) 
+        self.assertEqual(calculator.add(a, b), 4)
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+if __name__ == "__main__":
+    unittest.main()
